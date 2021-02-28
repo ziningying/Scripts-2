5116dd9859175f9048a4aabd49f01c3b1f7328ff
ziningying <ziningying@163.com>
2021-02-28 14:56:26 +0800

    Update jukan.js

--- Task/jukan.js
+++ Task/jukan.js
@@ -41,7 +41,9 @@ if ($.isNode()) {
 } else {
    BodyArr.push(bodys)
 }
+      console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
 
+      console.log(`============ 脚本执行-北京时间(UTC+8)：${JKbody})
 if ($.isNode()) {
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
